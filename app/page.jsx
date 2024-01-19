3b2b5c79a8d561b44b90f9eb08a68b437f6b8d5a
Mostafa Ranjbar <mostafaranjbar76@gmail.com>
2024-01-19 16:08:53 +0330

    add download section component

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@ import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
 import Category from "@/components/Home/Category/Category";
 import Cities from "@/components/Home/Cities/Cities";
+import DownloadSection from "@/components/Home/DownloadSection/DownloadSection";
 import VendorRegister from "@/components/Home/VendorRegister/VendorRegister";
 
 
@@ -11,6 +12,7 @@ export default function Home() {
       <Header/>
       <main className="p-4 pt-0 grow box-border w-full max-w-[85.375rem] mx-auto tablet:p-6 laptop:p-10">
         <Category/>
+        <DownloadSection/>
         <VendorRegister/>
       </main>
       <Cities/>
