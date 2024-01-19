f9ef5d5741be9c68bd524124ca852125de88e299
Mostafa Ranjbar <mostafaranjbar76@gmail.com>
2024-01-19 13:13:34 +0330

    add vendor register component

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@ import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
 import Category from "@/components/Home/Category/Category";
 import Cities from "@/components/Home/Cities/Cities";
+import VendorRegister from "@/components/Home/VendorRegister/VendorRegister";
 
 
 export default function Home() {
@@ -10,7 +11,7 @@ export default function Home() {
       <Header/>
       <main className="p-4 pt-0 grow box-border w-full max-w-[85.375rem] mx-auto tablet:p-6 laptop:p-10">
         <Category/>
-        
+        <VendorRegister/>
       </main>
       <Cities/>
       <Footer/>
