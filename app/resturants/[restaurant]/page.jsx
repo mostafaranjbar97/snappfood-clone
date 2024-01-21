ecfe7b3607a0df1049019046279e324bedd1d68a
Mostafa Ranjbar <mostafaranjbar76@gmail.com>
2024-01-21 21:39:10 +0330

    add  restaurant page

---
+++ app/resturants/[restaurant]/page.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+function Resturants() {
+  return (
+    
+        <main className='pt-[4.25rem] px-4 pb-4 grow box-border w-full max-w-[85.375rem] mx-auto'>
+           <div className='box-border w-[calc(100%+2rem)] -m-4 flex flex-wrap'></div>
+        </main>
+    
+  )
+}
+
+export default Resturants
---
+++ app/resturants/layout.jsx
@@ -0,0 +1,22 @@
+import BreadCrumbs from "@/components/Resturants/BreadCrumbs"
+
+
+
+
+
+
+export const metadata = {
+  title: ' اسنپ فود | سفارش آنلاین غذا از تمامی رستوران ها و فست فودها',
+  description: 'سفارش غذا با تخفیف از رستوران ها و فست فود های ایران. ارسال اکسپرس در سریع ترین زمان ممکن. خرید غذا با امکان مقایسه رستوران ها',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fa">
+      <body>
+        <BreadCrumbs/>
+        {children}
+      </body>
+    </html>
+  )
+}
