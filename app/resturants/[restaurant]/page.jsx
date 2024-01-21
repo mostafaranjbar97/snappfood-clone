cc69c30d7a62bca942ffaffab6ec89b00687d4d2
Mostafa Ranjbar <mostafaranjbar76@gmail.com>
2024-01-21 22:59:43 +0330

    add vendor info sidebar

--- app/resturants/[restaurant]/page.jsx
+++ app/resturants/[restaurant]/page.jsx
@@ -1,11 +1,14 @@
 
+import RestaurantSideBar from '@/components/Restaurant/RestaurantSideBar'
 import React from 'react'
 
 function Resturants() {
   return (
     
         <main className='pt-[4.25rem] px-4 pb-4 grow box-border w-full max-w-[85.375rem] mx-auto'>
-           <div className='box-border w-[calc(100%+2rem)] -m-4 flex flex-wrap'></div>
+           <div className='box-border w-[calc(100%+2rem)] -m-4 flex flex-wrap'>
+                <RestaurantSideBar/>
+           </div>
         </main>
     
   )
--- components/Restaurant/RestaurantSideBar.jsx
+++ components/Restaurant/RestaurantSideBar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import SideVendorInfo from './SideVendorInfo'
 
 function RestaurantSideBar() {
   return (
     <aside className='max-w-full basis-full box-border p-4 tablet:max-w-[50%] tablet:basis-6/12 laptop:max-w-[25%] laptop:basis-3/12'>
         <div className='sticky top-[5.5rem] transition-[top] duration-[350ms]'>
-            
+            <SideVendorInfo/>
         </div>
     </aside>
   )
