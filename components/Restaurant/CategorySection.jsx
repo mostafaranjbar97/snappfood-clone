d0bc8e60b021937a79a13f37d1e3a346b9a13f0b
Mostafa Ranjbar <mostafaranjbar76@gmail.com>
2024-01-22 00:08:26 +0330

    add category section food in restaurant page

---
+++ components/Restaurant/CategorySection.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function CategorySection() {
+  return (
+    <section className='scroll-mt-[4.375rem]'>
+        <p className='h-12 text-carbon-light font-bold text-xs flex justify-center items-center '>پر‌طرفدارها</p>
+        <div className='border-t-border-xs border-t-surface-dark w-full box-border flx flex-wrap'>
+            
+        </div>
+    </section>
+  )
+}
+
+export default CategorySection
