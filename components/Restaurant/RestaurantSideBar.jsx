ab5b47c0840dea82994f24e2607f478c2a3d3b93
Mostafa Ranjbar <mostafaranjbar76@gmail.com>
2024-01-21 21:44:24 +0330

    add restaurant sidebar

---
+++ components/Restaurant/RestaurantSideBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function RestaurantSideBar() {
+  return (
+    <aside className='max-w-full basis-full box-border p-4 tablet:max-w-[50%] tablet:basis-6/12 laptop:max-w-[25%] laptop:basis-3/12'>
+        <div className='sticky top-[5.5rem] transition-[top] duration-[350ms]'>
+            
+        </div>
+    </aside>
+  )
+}
+
+export default RestaurantSideBar
