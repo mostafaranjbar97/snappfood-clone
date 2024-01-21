e234316a817b87f35c0c1f403b9101bdb1abc0d2
Mostafa Ranjbar <mostafaranjbar76@gmail.com>
2024-01-22 00:02:27 +0330

    add category section container

---
+++ components/Restaurant/CategorySectionsContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CategorySectionsContainer() {
+  return (
+    <section className='bg-white border-border-xs border-surface-dark rounded-lg'>
+        
+    </section>
+  )
+}
+
+export default CategorySectionsContainer
