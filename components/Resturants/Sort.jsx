faafa774a1ded1ad41390c88c37ecfe5403888af
Mostafa Ranjbar <mostafaranjbar76@gmail.com>
2024-01-21 07:33:25 +0330

    add sort btn to resturant page

--- components/Resturants/Sort.jsx
+++ components/Resturants/Sort.jsx
@@ -26,7 +26,7 @@ function Sort() {
                 </div>
                 <div className='flex items-center self-stretch shrink-0 box-border'>
                     <div className='text-[rgb(204,204,204)] flex p-2 transition-[color] box-border hover:text-[rgb(153,153,153)]'>
-                        <svg height="20" width="20" viewBox="0 0 20 20" aria-hidden="true" focusable="false" class="css-8mmkcg"><path d="M14.348 14.849c-0.469 0.469-1.229 0.469-1.697 0l-2.651-3.030-2.651 3.029c-0.469 0.469-1.229 0.469-1.697 0-0.469-0.469-0.469-1.229 0-1.697l2.758-3.15-2.759-3.152c-0.469-0.469-0.469-1.228 0-1.697s1.228-0.469 1.697 0l2.652 3.031 2.651-3.031c0.469-0.469 1.228-0.469 1.697 0s0.469 1.229 0 1.697l-2.758 3.152 2.758 3.15c0.469 0.469 0.469 1.229 0 1.698z"></path></svg>
+                        <svg height="20" width="20" viewBox="0 0 20 20" aria-hidden="true" focusable="false" className="css-8mmkcg"><path d="M14.348 14.849c-0.469 0.469-1.229 0.469-1.697 0l-2.651-3.030-2.651 3.029c-0.469 0.469-1.229 0.469-1.697 0-0.469-0.469-0.469-1.229 0-1.697l2.758-3.15-2.759-3.152c-0.469-0.469-0.469-1.228 0-1.697s1.228-0.469 1.697 0l2.652 3.031 2.651-3.031c0.469-0.469 1.228-0.469 1.697 0s0.469 1.229 0 1.697l-2.758 3.152 2.758 3.15c0.469 0.469 0.469 1.229 0 1.698z"></path></svg>
                     </div>
                     <span className='self-stretch my-2 w-px box-border bg-[rgb(204,204,204)]'></span>
                     <div className='flex transition-[color] text-[rgb(204,204,204)] p-2'>
@@ -34,7 +34,7 @@ function Sort() {
                     </div>
                 </div>
             </div>
-            <div className='absolute top-full bg-white rounded my-2 w-[200px] box-border shadow-[rgba(0,0,0,0.1)_0px_0px_0px_1px,rgba(0,0,0,0.1)_0px_4px_11px]'>
+            <div className='hidden absolute top-full bg-white rounded my-2 w-[200px] box-border shadow-[rgba(0,0,0,0.1)_0px_0px_0px_1px,rgba(0,0,0,0.1)_0px_4px_11px]'>
                 <div className='max-h-[300px] overflow-y-auto py-1 relative'>
                     <div className='bg-transparent py-2 px-3 w-full box-border select-none hover:bg-[rgb(222,235,255)] active:bg-[rgb(178,212,255)] focus:bg-[rgb(38,132,255)] focus:text-white'>بالاترین امتیاز</div>
                     <div className='bg-transparent py-2 px-3 w-full box-border select-none hover:bg-[rgb(222,235,255)] active:bg-[rgb(178,212,255)] focus:bg-[rgb(38,132,255)] focus:text-white'>نزدیک‌ترین</div>
