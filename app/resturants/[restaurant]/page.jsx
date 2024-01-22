d971c26c9ad88cee86f504dc662082494c2c4beb
Mostafa Ranjbar <mostafaranjbar76@gmail.com>
2024-01-22 07:13:55 +0330

    change name of res sidebar to right res sidebar

--- app/resturants/[restaurant]/page.jsx
+++ app/resturants/[restaurant]/page.jsx
@@ -1,6 +1,6 @@
 
 import CategorySectionsContainer from '@/components/Restaurant/CategorySectionsContainer'
-import RestaurantSideBar from '@/components/Restaurant/RestaurantSideBar'
+import RightRestaurantSideBar from '@/components/Restaurant/RightRestaurantSideBar'
 import React from 'react'
 
 function Resturants() {
@@ -8,7 +8,7 @@ function Resturants() {
     
         <main className='pt-[4.25rem] px-4 pb-4 grow box-border w-full max-w-[85.375rem] mx-auto'>
            <div className='box-border w-[calc(100%+2rem)] -m-4 flex flex-wrap'>
-                <RestaurantSideBar/>
+                <RightRestaurantSideBar/>
                 <section className='max-w-full basis-full box-border p-4 tablet:max-w-[50%] tablet:basis-6/12 ' >
                     <CategorySectionsContainer/>
                 </section>
