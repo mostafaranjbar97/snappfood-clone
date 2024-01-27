49307350285e8cf7f8d8fd95eec71509240e345e
Mostafa Ranjbar <mostafaranjbar76@gmail.com>
2024-01-28 00:59:54 +0330

    install framer motion

--- components/Restaurant/SideFoodCategory.jsx
+++ components/Restaurant/SideFoodCategory.jsx
@@ -4,7 +4,6 @@ import clsx from 'clsx'
 import Image from 'next/image'
 import { useParams } from 'next/navigation'
 import React, { useState } from 'react'
-import { useInView } from 'react-intersection-observer'
 import { useDispatch } from 'react-redux'
 
 function SideFoodCategory({restaurants}) {
@@ -21,9 +20,6 @@ function SideFoodCategory({restaurants}) {
     dispatch(setSelectCategory(e.target.id))
   }
 
-  const [section1Ref, section1InView] = useInView({ threshold: 0 });
-  const [section2Ref, section2InView] = useInView({ threshold: 0 });
-  console.log("section1InView",section1InView,"section2InView",section2InView)
   return (
     <nav dir='rtl' className='h-[30vh] min-h-[9.375rem] overflow-y-auto flex flex-col laptop:h-[calc((100vh-180px)-152px)] ltr-laptop'>
 
@@ -31,7 +27,7 @@ function SideFoodCategory({restaurants}) {
           restaurant.restCatsMenu.map((cat)=>{
             if(cat.name=="فودپارتی"){
               return (
-                <p ref={section1Ref} id={cat.id} key={cat.id} onClick={selectCatHandler} className={clsx('mb-4 py-0.5 px-3 text-sm cursor-pointer font-iransans inline-block ',selectCat==cat.id ? "border-r-2 border-r-carbon-main laptop:border-l-carbon-main  laptop:border-l-2 laptop:border-r-0 font-bold text-carbon-main" : "text-carbon-light")}>
+                <p id={cat.id} key={cat.id} onClick={selectCatHandler} className={clsx('mb-4 py-0.5 px-3 text-sm cursor-pointer font-iransans inline-block ',selectCat==cat.id ? "border-r-2 border-r-carbon-main laptop:border-l-carbon-main  laptop:border-l-2 laptop:border-r-0 font-bold text-carbon-main" : "text-carbon-light")}>
                   <span id={cat.id} className='relative flex items-center'>فود پارتی
                       <Image  id={cat.id} width={20} height={20} src={"/icons/restaurant/foodparty.svg"} className='ml-2'/> 
                   </span>
@@ -39,7 +35,7 @@ function SideFoodCategory({restaurants}) {
               )
             }else if(cat.name=="کوپن‌ها"){
               return (
-                <p ref={section2Ref} id={cat.id} key={cat.id} onClick={selectCatHandler} className={clsx('mb-4 py-0.5 px-3 text-sm cursor-pointer font-iransans inline-block ',selectCat==cat.id ? "border-r-2 border-r-carbon-main laptop:border-l-carbon-main  laptop:border-l-2 laptop:border-r-0 font-bold text-carbon-main" : "text-carbon-light")}>
+                <p id={cat.id} key={cat.id} onClick={selectCatHandler} className={clsx('mb-4 py-0.5 px-3 text-sm cursor-pointer font-iransans inline-block ',selectCat==cat.id ? "border-r-2 border-r-carbon-main laptop:border-l-carbon-main  laptop:border-l-2 laptop:border-r-0 font-bold text-carbon-main" : "text-carbon-light")}>
                   <span id={cat.id} className='relative flex items-center  '>کوپن‌ها
                       <Image  id={cat.id} width={20} height={20} src={"/icons/restaurant/coupon.svg"} className='ml-2'/>
                       <span className='absolute -top-2 right-[4.25rem] w-[18px] h-[18px] bg-accent2-main border-2 border-white rounded-3xl flex flex-col items-center laptop:left-[4.25rem]'>
