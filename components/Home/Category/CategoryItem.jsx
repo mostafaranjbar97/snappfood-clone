ddfb2435ee5bf865c79c1897065ed72db634c168
Mostafa Ranjbar <mostafaranjbar76@gmail.com>
2024-01-29 21:17:06 +0330

    when click on category navigate to restaurant page with category id

--- components/Home/Category/CategoryItem.jsx
+++ components/Home/Category/CategoryItem.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 function CategoryItem({data}) {
   return (
   <div className="basis-1/2 max-w-[50%] p-4 box-border tablet:basis-1/3 tablet:max-w-[1/3] laptop:basis-1/4 laptop:max-w-[1/4] desktop:basis-1/6 desktop:max-w-[1/6] ">
-    <Link href={`/resturants/?category=${data.id}`} className="bg-transparent">
+    <Link href={`/resturants/?cat=${data.id}`} className="bg-transparent">
       <div className="relative p-[0.1875rem] h-[5.9375rem] border border-white rounded-xl shadow-shadows-high cursor-pointer transition-all ease-in-out duration-200 flex bg-white hover:shadow-shadows-medium">
         <img src={data.img}  alt={data.name} className="rounded-xl w-full h-full object-cover" />
         <div className="absolute right-0 bottom-0 py-1 pr-4 pl-[0.8125rem] rounded-tl-xl rounded-br-xl bg-white flex items-center cursor-pointer transition-all ease-in-out duration-200 ">
