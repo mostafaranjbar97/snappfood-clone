1479e2fd05ff2cc9508cf36dc7b330abe1be5f71
Mostafa Ranjbar <mostafaranjbar76@gmail.com>
2024-02-02 09:57:30 +0330

    fix style of sort btn

--- components/Modal/VendorDetail.jsx
+++ components/Modal/VendorDetail.jsx
@@ -72,10 +72,10 @@ function VendorDetail({restaurant}) {
                 <span className='ml-2.5 font-iransans font-bold text-xs inline-block text-carbon-main'>به ترتیب</span>
                 <div className='relative bg-carbon-alphaLight rounded-xl cursor-pointer flex items-center'>
                     {/* <div className='absolute h-9 mr-0.5 bg-white rounded-xlg transition-all duration-100 w-[50%] translate-x-[-133px]'></div> */}
-                    <div onClick={sortHandler} id='1' className={clsx('z-[2] w-[7.3125rem] p-2.5 flex justify-center items-center border-2 border-carbon-alphaLight rounded-xl ',sort==1 ? 'text-accent2-main bg-white' : 'text-carbon-main bg-transparent')}>
+                    <div onClick={sortHandler} id='1' className={clsx('z-[2] w-[7.3125rem] p-2.5 flex justify-center items-center border-2 border-carbon-alphaLight rounded-xl ',sort==1 ? 'text-accent2-main bg-white' : 'text-carbon-main bg-transparent border-transparent')}>
                         <p className='font-iransans text-sm inline-block '>جدیدترین</p>
                     </div>
-                    <div onClick={sortHandler} id='2' className={clsx('z-[2] w-[7.3125rem] p-2.5 flex justify-center items-center border-2 border-carbon-alphaLight rounded-xl ',sort==2 ? 'text-accent2-main bg-white' : 'text-carbon-main  bg-transparent')}>
+                    <div onClick={sortHandler} id='2' className={clsx('z-[2] w-[7.3125rem] p-2.5 flex justify-center items-center border-2 border-carbon-alphaLight rounded-xl ',sort==2 ? 'text-accent2-main bg-white' : 'text-carbon-main  bg-transparent border-transparent')}>
                         <p className='font-iransans text-sm inline-block '>مرتبط‌ترین</p>
                     </div>
                 </div>
