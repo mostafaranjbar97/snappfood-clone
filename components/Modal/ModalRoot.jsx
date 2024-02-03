8deeb7fd532f9d4c571820fa911fb0ec23b693ac
Mostafa Ranjbar <mostafaranjbar76@gmail.com>
2024-02-03 12:05:09 +0330

    modal root change to server component

--- components/Modal/ModalRoot.jsx
+++ components/Modal/ModalRoot.jsx
@@ -1,8 +1,7 @@
-'use client'
+
 import React from 'react'
 import RestaurantInfoModal from './RestaurantInfoModal'
 import FoodInfoModal from './FoodInfoModal'
-import { useSelector } from 'react-redux'
 import FoodPartyInfoModal from './FoodPartyInfoModal'
 import AddressModal from './AddressModal'
 import SearchModal from './SearchModal'
@@ -15,33 +14,13 @@ import RestaurantMapModal from './RestaurantMapModal'
 
 function ModalRoot({restaurants,restaurantCategory}) {
 
-  const openFoodInfoModal=useSelector((store)=>store.openFoodInfoModal)
-  const {isOpen,foodId,catId,resId}=openFoodInfoModal
-  const {cart}=useSelector((store)=> store.cart)
-  let food={}
-  let foodCount=0
-  if(isOpen){
-    const restaurant=restaurants.filter((res)=>res.id==resId)[0]
-    const restaurantfood= restaurant.foods.filter((food)=>food.categoryId.includes(catId))
-    food =restaurantfood.filter((food)=>food.id==foodId)[0]
-    if(cart.filter((item)=>item.id==food.id).length>0){
-      foodCount=cart.filter((item)=> item.id == food.id)[0].count;
-  }
-  }
-
-  const showSearchModal=useSelector((store)=>store.searchModal)
-
-
-
-
-
   return (
     <div>
         <RestaurantInfoModal restaurants={restaurants} />
-        <FoodInfoModal food={food} isOpen={isOpen} foodCount={foodCount}/>
+        <FoodInfoModal restaurants={restaurants} />
         <FoodPartyInfoModal restaurants={restaurants}/>
         <AddressModal/>
-        <SearchModal showSearchModal={showSearchModal} restaurants={restaurants} restaurantCategory={restaurantCategory}/>
+        <SearchModal restaurants={restaurants} restaurantCategory={restaurantCategory}/>
         <OrderModal />
         <OrderCommentModal/>
         <OrderInvoiceModal/>
