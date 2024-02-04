20ee672c458922a8d845bbfeecd87c1b5ae1442d
Mostafa Ranjbar <mostafaranjbar76@gmail.com>
2024-02-04 11:10:15 +0330

    add filterlist and map on it to show filter options in sidebar

--- components/Resturants/SideFilter.jsx
+++ components/Resturants/SideFilter.jsx
@@ -32,57 +32,30 @@ function SideFilter() {
             }
       }
     
+      const filterList=
+      [{name:"پیک اسنپ‌فود",query:"has_express"},
+      {name:"دارای کوپن",query:"has_coupon"},
+      {name:"دارای تخفیف",query:"has_discount"},
+      {name:"فودپرو",query:"is_vip"},
+      {name:"به‌صرفه",query:"is_economical"},
+      {name:"خوش‌قیمت",query:"is_eco"}]
   return (
     <div className='mb-2 p-4 border border-carbon-alphaLight rounded-xl shadow-shadows-small flex'>
         <div className='grow flex flex-col flex-wrap'>
 
-            <div onClick={()=>queryHandler("has_express")} className='border-b border-b-carbon-alphaLight min-h-[3.4375rem] flex justify-between items-center'>
-                <span className='font-iransans text-sm text-carbon-main inline-block'>پیک اسنپ‌فود</span>
-                <label htmlFor="checkbox" className='flex items-center select-none ltr' >
-                    <input name='checkbox' type="checkbox" className='hidden p-0 box-border' />
-                    <span className={clsx('outline-0 inline-block w-10 h-6 relative cursor-pointer select-none  rounded-3xl p-0.5 transition-all duration-[400ms] after:rounded-[50%] after:bg-white after:transition-all after:duration-200  after:relative after:size-5 after:block',searchParams.has("filters","has_express")?"after:left-4 bg-accent2-main" :"bg-carbon-alphaLight after:left-0")}></span>
-                </label>
-            </div>
-
-            <div onClick={()=>queryHandler("has_coupon")} className='border-b border-b-carbon-alphaLight min-h-[3.4375rem] flex justify-between items-center'>
-                <span className='font-iransans text-sm text-carbon-main inline-block'>دارای کوپن</span>
-                <label htmlFor="" className='flex items-center select-none ltr'>
-                    <input type="checkbox" className='hidden p-0 box-border' />
-                    <span className={clsx('outline-0 inline-block w-10 h-6 relative cursor-pointer select-none  rounded-3xl p-0.5 transition-all duration-[400ms] after:rounded-[50%] after:bg-white after:transition-all after:duration-200 after:relative after:size-5 after:block',searchParams.has("filters","has_coupon")?"after:left-4 bg-accent2-main" :"bg-carbon-alphaLight after:left-0")}></span>
-                </label>
-            </div>
-
-            <div onClick={()=>queryHandler("has_discount")} className='border-b border-b-carbon-alphaLight min-h-[3.4375rem] flex justify-between items-center'>
-                <span className='font-iransans text-sm text-carbon-main inline-block'>دارای تخفیف</span>
-                <label htmlFor="" className='flex items-center select-none ltr '>
-                    <input type="checkbox" className='hidden p-0 box-border' />
-                    <span className={clsx('outline-0 inline-block w-10 h-6 relative cursor-pointer select-none rounded-3xl p-0.5 transition-all duration-[400ms] after:rounded-[50%] after:bg-white after:transition-all after:duration-200 after:relative after:size-5 after:block',searchParams.has("filters","has_discount")?"after:left-4 bg-accent2-main" :"bg-carbon-alphaLight after:left-0")}></span>
-                </label>
-            </div>
-
-            <div onClick={()=>queryHandler("is_vip")} className='border-b border-b-carbon-alphaLight min-h-[3.4375rem] flex justify-between items-center'>
-                <span className='font-iransans text-sm text-carbon-main inline-block'>فودپرو</span>
-                <label htmlFor="" className='flex items-center select-none ltr '>
-                    <input type="checkbox" className='hidden p-0 box-border' />
-                    <span className={clsx('outline-0 inline-block w-10 h-6 relative cursor-pointer select-none  rounded-3xl p-0.5 transition-all duration-[400ms] after:rounded-[50%] after:bg-white after:transition-all after:duration-200 after:relative after:size-5 after:block',searchParams.has("filters","is_vip")?"after:left-4 bg-accent2-main" :"bg-carbon-alphaLight after:left-0")}></span>
-                </label>
-            </div>
-
-            <div onClick={()=>queryHandler("is_economical")} className='border-b border-b-carbon-alphaLight min-h-[3.4375rem] flex justify-between items-center'>
-                <span className='font-iransans text-sm text-carbon-main inline-block'>به‌صرفه</span>
-                <label htmlFor="" className='flex items-center select-none ltr '>
-                    <input type="checkbox" className='hidden p-0 box-border' />
-                    <span className={clsx('outline-0 inline-block w-10 h-6 relative cursor-pointer select-none rounded-3xl p-0.5 transition-all duration-[400ms] after:rounded-[50%] after:bg-white after:transition-all after:duration-200 after:relative after:size-5 after:block',searchParams.has("filters","is_economical")?"after:left-4 bg-accent2-main" :"bg-carbon-alphaLight after:left-0")}></span>
-                </label>
-            </div>
-
-            <div onClick={()=>queryHandler("is_eco")} className=' min-h-[3.4375rem] flex justify-between items-center'>
-                <span className='font-iransans text-sm text-carbon-main inline-block'>خوش‌قیمت</span>
-                <label htmlFor="" className='flex items-center select-none ltr'>
-                    <input type="checkbox" className='hidden p-0 box-border' />
-                    <span className={clsx('outline-0 inline-block w-10 h-6 relative cursor-pointer select-none  rounded-3xl p-0.5 transition-all duration-[400ms] after:rounded-[50%] after:bg-white after:transition-all after:duration-200 after:relative after:size-5 after:block',searchParams.has("filters","is_eco")?"after:left-4 bg-accent2-main" :"bg-carbon-alphaLight after:left-0")}></span>
-                </label>
-            </div>
+            {
+                filterList.map((filterItem)=>{
+                    return(
+                        <div onClick={()=>queryHandler(filterItem.query)} className='border-b border-b-carbon-alphaLight min-h-[3.4375rem] flex justify-between items-center'>
+                            <span className='font-iransans text-sm text-carbon-main inline-block'>{filterItem.name}</span>
+                            <label htmlFor="checkbox" className='flex items-center select-none ltr' >
+                                <input name='checkbox' type="checkbox" className='hidden p-0 box-border' />
+                                <span className={clsx('outline-0 inline-block w-10 h-6 relative cursor-pointer select-none  rounded-3xl p-0.5 transition-all duration-[400ms] after:rounded-[50%] after:bg-white after:transition-all after:duration-200  after:relative after:size-5 after:block',searchParams.has("filters",filterItem.query)?"after:left-4 bg-accent2-main" :"bg-carbon-alphaLight after:left-0")}></span>
+                            </label>
+                        </div>
+                    )
+                })
+            }
 
         </div>
     </div>
