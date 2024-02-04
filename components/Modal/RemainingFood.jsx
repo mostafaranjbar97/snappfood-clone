78fdab1256eac09e62e069eaa0799fab31d7ca20
Mostafa Ranjbar <mostafaranjbar76@gmail.com>
2024-02-04 06:44:57 +0330

    add margin bottom 0.5rem

--- components/Modal/RemainingFood.jsx
+++ components/Modal/RemainingFood.jsx
@@ -8,7 +8,7 @@ function RemainingFood({food}) {
             <p className={clsx('mb-2 font-bold font-iransans text-sm inline-block',food.numberRemaining > 3 ? "text-carbon-main" : "text-alert-main" )}>{food.numberRemaining}</p>
             <p className={clsx('mb-2 font-iransans text-sm inline-block mr-1',food.numberRemaining > 3 ? "text-carbon-main" : "text-alert-main" )}>عدد باقی مانده</p>
         </div>
-        <div className={clsx('h-0.5 w-full',food.numberRemaining > 3 ? "bg-inactive-dark" : "bg-alert-light" )}></div>
+        <div className={clsx('mb-2 h-0.5 w-full',food.numberRemaining > 3 ? "bg-inactive-dark" : "bg-alert-light" )}></div>
     </>
   )
 }
