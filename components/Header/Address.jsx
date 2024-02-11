c445ee0869ba6a20178b1553c8ca06ad9b53bff0
Mostafa Ranjbar <mostafaranjbar76@gmail.com>
2024-02-11 23:48:48 +0330

    fix style of address arrow

--- components/Header/Address.jsx
+++ components/Header/Address.jsx
@@ -18,7 +18,7 @@ function Address() {
             <p className='font-iransans font-bold text-sm inline-block text-carbon-main'></p>
             <div className='flex justify-center '>
                 <span className='font-iransans min-w-[29vw] max-w-[29vw] pt-0.5 overflow-hidden text-ellipsis opacity-60 whitespace-nowrap font-normal text-2xs leading-3 inline-block text-carbon-main tablet:max-w-[calc(57vw-85px)] laptop:min-w-[9.375rem] laptop:max-w-[calc(24vw-85px)]'>{address}</span>
-                <Image width={12} height={9} src={"/icons/header/bottom-purple.svg"} alt='bottom' className='mt-1 mr-[0.6rem] '/>
+                <Image width={12} height={9} src={"/icons/header/bottom-purple.svg"} alt='bottom' className=' mr-[0.6rem] '/>
             </div>
         </div>
     </div>
